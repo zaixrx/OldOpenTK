59a596465ed11c4493a21e31ba7a6ebbb5b7f4d2
zaixrx <102376766+zaixrx@users.noreply.github.com>
2024-01-07 21:35:51 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+namespace Physics
+{
+    class Program
+    {
+        private static void Main()
+        {
+            using (Window window = new Window(400, 600, "Physics"))
+            {
+                window.Run();
+            }
+        }
+    }
+}
